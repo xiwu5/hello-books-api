0b0586a2cf8f037262016581613c4890fd61c896
Xi Wu <youngjoycewu@gmail.com>
2025-10-17 19:07:54 -0700

    finished handlingbook_bp erros

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,17 +1,34 @@
-# from flask import Blueprint
-# from app.models.book import books
+from flask import Blueprint
+from app.models.book import books
 
-# books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-# @books_bp.get("")
-# def get_all_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append(
-#             {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
-#         )
-#     return books_response
+@books_bp.get("")
+def get_all_books():
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return books_response
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    try:
+        book_id = int(book_id)
+    except ValueError:
+        return {"message": "book_id must be an integer"}, 400
+    
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+            }
+        
+    return {"message": f"book {book_id} not found"}, 404
