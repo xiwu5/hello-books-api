59cdb820e8dde55af2d96c14060cf4cd75a5b48f
Xi Wu <youngjoycewu@gmail.com>
2025-10-28 16:27:07 +0800

    Updated  get one book

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -69,3 +69,40 @@ def get_all_books():
             }
         )
     return books_response
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response , 400))
+
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
+    
+    if not book:
+        response = {"message": f"book {book_id} not found"}
+        abort(make_response(response, 404))
+
+    return book
+
+# @books_bp.put("/<book_id>")
+# def update_book(book_id):
+#     book = validate_book(book_id)
+#     request_body = request.get_json()
+
+#     book.title = request_body["title"]
+#     book.description = request_body["description"]
+#     db.session.commit()
+
+#     return Response(status=204, mimetype="application/json")
