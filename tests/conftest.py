aa117ade53d333ba09b9f937499700e4414cb775
Xi Wu <youngjoycewu@gmail.com>
2025-10-30 12:41:47 +0800

    add on test:test_get_one_book()

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ from flask.signals import request_finished
 from dotenv import load_dotenv
 import os
 
+from app.models.book import Book
+
 load_dotenv()
 
 @pytest.fixture
@@ -30,3 +32,17 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_saved_books(app):
+    # Arrange
+    ocean_book = Book(title="Ocean Book",
+                    description="watr 4evr")
+    mountain_book = Book(title="Mountain Book",
+                        description="i luv 2 climb rocks")
+
+    db.session.add_all([ocean_book, mountain_book])
+    # Alternatively, we could do
+    # db.session.add(ocean_book)
+    # db.session.add(mountain_book)
+    db.session.commit()
