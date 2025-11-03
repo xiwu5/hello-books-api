68ab6f46d8c6de8346808e83c9fcf4c8a66ffeb1
Xi Wu <youngjoycewu@gmail.com>
2025-11-03 20:53:34 +0800

    delete unused lines

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -5,36 +5,6 @@ from ..db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-# @books_bp.get("")
-# def get_all_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append(
-#             {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
-#         )
-#     return books_response
-
-# @books_bp.get("/<book_id>")
-# def get_one_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except ValueError:
-#         return {"message": "book_id must be an integer"}, 400
-    
-#     for book in books:
-#         if book.id == book_id:
-#             return {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description,
-#             }
-        
-#     return {"message": f"book {book_id} not found"}, 404
-
 @books_bp.post("")
 def create_book():
     request_body = request.get_json()
