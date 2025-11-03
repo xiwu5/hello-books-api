fe099cb29a80ba57f35e1cb7206266dd4eb127e1
Xi Wu <youngjoycewu@gmail.com>
2025-11-03 22:07:55 +0800

    add on more tests

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -69,14 +69,14 @@ def validate_book(book_id):
     try:
         book_id = int(book_id)
     except:
-        response = {"message": f"book {book_id} invalid"}
+        response = {"message": f"Book {book_id} invalid"}
         abort(make_response(response , 400))
 
     query = db.select(Book).where(Book.id == book_id)
     book = db.session.scalar(query)
     
     if not book:
-        response = {"message": f"book {book_id} not found"}
+        response = {"message": f"Book {book_id} not found"}
         abort(make_response(response, 404))
 
     return book
